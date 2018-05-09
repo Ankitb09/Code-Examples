d7f44a86f107f806fa10d9243f8ae957ab167f8f
ankit <ankit.b09@gmail.com>
2018-05-09 17:05:55 +0530

    weather app

---
+++ weather-app/js/main.js
@@ -0,0 +1,35 @@
+window.onload = getGeoLocation;
+function geo_success(position) {
+    //do_something(position.coords.latitude, position.coords.longitude);
+    var url = `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',url);
+    xhr.onload = function(success){
+        console.log(success);
+        console.log(xhr)
+        console.log(xhr.response)
+    }
+    xhr.send();
+}
+
+function geo_error() {
+    alert("Sorry, no position available.");
+}
+
+var geo_options = {
+    enableHighAccuracy: true,
+    maximumAge: 4000,
+    timeout: 4000
+};
+
+
+function getGeoLocation() {
+    if ('geolocation' in navigator) {
+        var wpid = navigator.geolocation.getCurrentPosition(geo_success, geo_error, geo_options);
+        return wpid;
+    }
+    else {
+        alert('Geolocation not supported')
+    }
+}
