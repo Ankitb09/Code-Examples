d3641e6f557f16460d69b2cbaf94edbc054faa61
LCINDIA\Ankit.Jain <ankit.jain@bold.com>
2018-05-11 14:56:29 +0530

    weather app

--- weather-app/js/main.js
+++ weather-app/js/main.js
@@ -1,12 +1,25 @@
+// global elements
+
+var cityElem = document.querySelector('.city span');
+var countryElem = document.querySelector('.city small');
+var currTemp = document.querySelector('.currTemp');
+var weatherCond = document.querySelector('.description');
+var unitVal = document.getElementById('convert');
+var tempVal;
+
 window.onload = getGeoLocation;
 function geo_success(position) {
-    //do_something(position.coords.latitude, position.coords.longitude);
     var url = `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
 
     var xhr = new XMLHttpRequest();
-    xhr.open('GET',url);
-    xhr.onload = function(success){
-        console.log(JSON.parse(xhr.response))
+    xhr.open('GET', url);
+    xhr.onload = function (success) {
+        var resp = JSON.parse(xhr.response);
+        cityElem.innerHTML = resp.name;
+        countryElem.innerHTML = resp.sys.country;
+        tempVal = resp.main.temp;
+        currTemp.innerHTML = tempVal;
+        weatherCond.innerHTML = resp.weather[0].main;
     }
     xhr.send();
 }
@@ -29,5 +42,22 @@ function getGeoLocation() {
     }
     else {
         alert('Geolocation not supported')
+    }
+}
+
+document.getElementById('convert').addEventListener('click', function () {
+    var text = this.getAttribute('data-degree');
+    celToFaranheit(tempVal, text)
+})
+
+function celToFaranheit(temp, degree) {
+    if (degree == 'C') {
+        currTemp.innerHTML = (temp * 9 / 5 + 32);
+        document.getElementById('convert').setAttribute('data-degree', 'F')
+        unitVal.innerHTML = '&deg;F';
+    } else {
+        currTemp.innerHTML = tempVal;
+        document.getElementById('convert').setAttribute('data-degree', 'C')
+        unitVal.innerHTML = '&deg;C';
     }
 }
